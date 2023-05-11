7460138f95bbe109d3e1267145733090dbdbd7f1
deev <100467754+deev1207@users.noreply.github.com>
2023-05-11 15:19:45 +0530

    test

---
+++ app.component.ts
@@ -0,0 +1,165 @@
+import { OnInit, OnChanges, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
+import loader from '@monaco-editor/loader';
+import { HttpClient } from '@angular/common/http';
+import { io } from 'socket.io-client';
+import { Terminal } from 'xterm';
+import { WebLinksAddon } from 'xterm-addon-web-links';  
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnChanges{
+
+  constructor(private http: HttpClient){}
+
+  fontsize: number = 18;
+  tabsize: number = 4;
+  code: string= '//Type your code here.'; //Contains Code Inside the File.
+  fileHandle : any = null;
+  showResultBox = false;
+  finalResult: any;
+  isfileUploaded: boolean = false;
+  uploadFileName: string;
+  term:any = new Terminal();
+  tabNameVisible: boolean = false;
+  fileExtension: string = "";
+  SelectedLanguage: string = "C++";
+  showProcessingMsg: boolean = false;
+  showProcessingAiMsg:boolean=false;
+  processingMsg: any = "";
+  processingAiMsg: any = "";
+  runDisabled: boolean = true;
+  socket = io("http://localhost:3000")
+
+
+  ngOnInit(){
+    try{
+      this.code = localStorage.getItem('code');
+    }
+    catch{
+      this.code = '//Type your code here.'; //Contains Code Inside the File.
+    }
+    loader.config({
+      paths: {
+        vs: './',
+      }
+    });
+
+    this.socket.on('connect', ()=>{
+      console.log(`Connected with: ${this.socket.id}`)
+    })
+  }
+
+  ngOnChanges(){
+    console.log(this.code)
+    localStorage.setItem('code', this.code);
+  }
+
+  editorOptions = {theme: 'vs-dark', 
+                  language: 'cpp', 
+                  renderIndentGuides: true, 
+                  fontSize: this.fontsize, 
+                  tabSize: this.tabsize,
+                  wordWrap: "on"};
+
+
+  async uploadFileUsingFileSystemAPI(){
+    [this.fileHandle] = await (window as any).showOpenFilePicker();
+    const file: File = await this.fileHandle.getFile();
+    const contents = await file.text();
+    if(this.fileHandle != null){
+      this.isfileUploaded = true;
+      this.uploadFileName = this.fileHandle.name;
+      this.tabNameVisible = true
+      this.finalResult = "";
+      this.showResultBox = false;
+    }
+    this.code = contents;
+    localStorage.clear();
+    localStorage.setItem('code', this.code);
+
+  }
+
+  async saveFileUsingFileSystemAPI(){
+    const contents = this.code;
+    const writable = await this.fileHandle.createWritable();
+    await writable.write(contents);
+    await writable.close();
+  }
+
+  compileCode(){
+    this.term.open(document.getElementById('terminal'));
+    this.finalResult = "";
+    this.socket.emit('compile-code', this.code)
+
+    this.socket.on("compile-code-msg", async res => {
+      if(res == "Success!"){
+        this.runDisabled = false;
+        this.showProcessingMsg = true;
+        this.processingMsg = "Compilation Successful.";
+      }
+      else{
+        this.showProcessingMsg = true;
+        this.processingMsg = res;
+      }
+    })
+  }
+
+  runCode(){
+    this.term.reset()
+    this.showProcessingMsg = false;
+    this.processingMsg = "";
+    
+    this.socket.emit("run-code")
+
+    this.socket.on("code-output", output => {
+      this.term.write(output);
+    })
+
+    this.term.onKey((e) => {
+      console.log(e)
+      console.log(e.key)
+      if(e == 'keydown')
+        this.term.write(e.key);
+      this.socket.emit('send-input-value', e.key)
+    })
+  }
+
+  resetEditor(){
+    this.fileHandle = null;
+    this.finalResult = "";
+    this.showResultBox = false;
+    this.uploadFileName = "";
+    this.fileExtension = "";
+    window.location.href = window.location.href;
+  }
+
+  debug(){
+    this.socket.emit('debug-code', this.code);
+    this.socket.on('gpt-response', output => {
+      console.log('debug',output);
+    })
+
+  }
+
+  compileWithAi(){
+    this.term.open(document.getElementById('terminal'));
+    this.finalResult = "";
+    this.socket.emit('compile-Ai', this.code)
+
+    this.socket.on("compile-Ai-msg", async res => {
+      if(res == "Success!"){
+        this.runDisabled = false;
+        this.showProcessingAiMsg = true;
+        this.processingAiMsg = "Compilation Successful.";
+      }
+      else{
+        this.showProcessingAiMsg = true;
+        this.processingAiMsg = res;
+      }
+    })
+  }
+}
---
+++ server.ts
@@ -0,0 +1,146 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import fs from 'fs';
+const os = require("os");
+const pty = require("node-pty");
+const OpenAI = require('openai-api');
+
+const { exec } = require('child_process');
+const shell = os.platform() === "win32" ? "powershell.exe" : "bash";
+const openai = new OpenAI('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');
+
+const io = require('socket.io')(3000, {
+    cors: {
+        credentials: true,
+        origin: ['http://localhost:4200']
+    }
+})
+
+let i = 0;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+let ptyProcess = pty.spawn(shell, [], {
+    cwd: __dirname,
+    env: process.env
+});
+
+function createTempFileWithCode(code) {
+    return new Promise(function (resolve) {
+        fs.writeFile('TempFiles/tempCode.cpp', code, function (err) {
+            if (err) throw err;
+            resolve('File is created successfully.');
+        });
+    })
+}
+
+function createExectuableFile() {
+    return new Promise(function (resolve, reject) {
+        exec("g++ TempFiles/tempCode.cpp -o TempFiles/outputFile\r", (error, stdout, stderr) => {
+            if (error) {
+                console.log(error);
+                
+                reject(error.message);
+            }
+            else if (stderr) {
+                reject(stderr);
+            }
+            else {
+                resolve("Success!")
+            }
+        })
+    })
+}
+
+io.on('connection', async socket => {
+    console.log("Connection Formed!")
+    console.log(socket.id);
+
+    socket.on("compile-code", async (code) => {
+        await createTempFileWithCode(code);
+
+        await createExectuableFile()
+            .then(res => socket.emit("compile-code-msg", res))
+            .catch(err => socket.emit("compile-code-msg", err));
+        });
+        socket.on("run-code", () => {
+            ptyProcess.write("cls\r");
+            ptyProcess.write("./TempFiles/outputFile.exe\r");
+        })
+
+        ptyProcess.on('data', function (output) {
+            console.log(output)
+            socket.emit("code-output", output)
+        });
+
+        socket.on("send-input-value", (data) => {
+            ptyProcess.write(data);
+        });
+
+        socket.on("debug-code", async (code) => {
+            try {
+                const completion = await openai.complete({
+                    engine: 'text-davinci-003',
+                    prompt: `fix this c++ code \n${code}`,
+                    temperature: 0,
+                    maxTokens: 150,
+                    n: 1,
+                    frequency_penalty: 0,
+                    presence_penalty: 0.6,
+                    stop: [" Human:", " AI:"]
+                });
+
+                const { choices } = completion.data;
+                socket.emit('gpt-response', choices[0].text);
+                console.log(choices[0].text);
+            } catch (error) {
+                console.log(error);
+                socket.emit('gpt-response', error);
+            }
+        })
+        socket.on("compile-Ai", async (code) => {
+            await createTempFileWithCode(code);
+    
+            await createExectuableFile()
+                .then(res => socket.emit("compile-Ai-msg", res))
+                .catch(async err => {
+                    console.log(err);
+                    
+                    const completion = await openai.complete({
+    
+                        engine: 'text-davinci-003',
+    
+                        prompt: `explain the error in simple terms and show the line number where the error occured: ${err}`,
+    
+                        temperature: 0,
+    
+                        maxTokens: 150,
+    
+                        n: 1,
+    
+                        frequency_penalty: 0,
+    
+                        presence_penalty: 0.6,
+    
+                        stop: [" Human:", " AI:"]
+    
+                    });
+    
+    
+    
+                    const { choices } = completion.data;
+    
+                    socket.emit('compile-Ai-msg', choices[0].text);
+    
+                    console.log(choices[0].text);
+    
+                });
+            });
+    })
+
